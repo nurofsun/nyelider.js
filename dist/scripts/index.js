8ae45fe943954aca89ac260f48a4715cd2f6093d
nurofsun <nurrahmat2050@gmail.com>
2020-04-30 12:55:30 +0700

    Setup dev environment

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Vlider = /*#__PURE__*/function () {
+  function Vlider(options) {
+    _classCallCheck(this, Vlider);
+
+    this.options = {
+      size: 1,
+      direction: 'horizontal',
+      el: '.vlider'
+    };
+  }
+
+  _createClass(Vlider, [{
+    key: "init",
+    value: function init() {
+      var vlider = document.querySelector(el);
+    }
+  }]);
+
+  return Vlider;
+}();
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+class Vlider {
+    constructor(options) {
+        this.options = {
+            size: 1,
+            direction: 'horizontal',
+            el: '.vlider'
+        }
+    }
+    init() {
+        let vlider = document.querySelector(el);
+    }
+}

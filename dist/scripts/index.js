cda9e70d98e90eb35fae60d0d5b75b35b19a02ef
nurofsun <nurrahmat2050@gmail.com>
2020-04-30 20:05:49 +0700

    Basic feature 'horizontal' direction

--- dist/scripts/index.js
+++ dist/scripts/index.js
@@ -7,20 +7,83 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var Vlider = /*#__PURE__*/function () {
-  function Vlider(options) {
+  function Vlider(_ref) {
+    var _ref$size = _ref.size,
+        size = _ref$size === void 0 ? 3 : _ref$size,
+        _ref$direction = _ref.direction,
+        direction = _ref$direction === void 0 ? 'horizontal' : _ref$direction,
+        _ref$el = _ref.el,
+        el = _ref$el === void 0 ? '.vlider' : _ref$el,
+        _ref$withIndicator = _ref.withIndicator,
+        withIndicator = _ref$withIndicator === void 0 ? false : _ref$withIndicator;
+
     _classCallCheck(this, Vlider);
 
-    this.options = {
-      size: 1,
-      direction: 'horizontal',
-      el: '.vlider'
-    };
+    this.size = size;
+    this.direction = direction;
+    this.el = el;
+    this.withIndicator = withIndicator;
   }
 
   _createClass(Vlider, [{
     key: "init",
     value: function init() {
-      var vlider = document.querySelector(el);
+      var _this = this;
+
+      var vlider = document.querySelector(this.el),
+          vliderGrid = document.querySelector('.vlider-grid'),
+          vliderPosition = 0,
+          vliderItems = document.querySelectorAll('.vlider-item'),
+          vliderPerClickMove = 100 / this.size,
+          vliderLimit = vliderPerClickMove * vliderItems.length - vliderPerClickMove * this.size,
+          vliderNext = document.querySelector('.vlider-next'),
+          vliderPrev = document.querySelector('.vlider-prev'); // initialize the size of slider items
+
+      vliderItems.forEach(function (item) {
+        item.style.flexBasis = 100 / _this.size + '%';
+      }); // function to make vlider indicator icon, and it's clickable
+
+      var enableIndicator = function enableIndicator() {
+        var result = '';
+
+        for (var i = 0; i < _this.size; i++) {
+          result += "<span class=\"icon\"></span>";
+        }
+
+        return result;
+      };
+
+      if (this.withIndicator) {
+        document.querySelector('.vlider-indicator').innerHTML = enableIndicator();
+        var vliderIndicators = document.querySelectorAll('.vlider-indicator > .icon');
+        vliderIndicators.forEach(function (indicator, index) {
+          indicator.addEventListener('click', function (event) {
+            vliderPosition = vliderPerClickMove * _this.size * index;
+            vliderGrid.style.right = vliderPosition + '%';
+          });
+        });
+      } // just for debugging
+
+
+      console.log("\n            element: ".concat(this.el, ",\n            direction: ").concat(this.direction, ",\n            size: ").concat(this.size, "\n        "));
+      vliderNext.addEventListener('click', function (event) {
+        if (vliderPosition >= vliderLimit) {
+          vliderPosition = 0;
+        } else {
+          vliderPosition += vliderPerClickMove;
+        }
+
+        vliderGrid.style.right = vliderPosition + '%';
+      });
+      vliderPrev.addEventListener('click', function (event) {
+        if (vliderPosition <= 0) {
+          vliderPosition = vliderLimit;
+        } else {
+          vliderPosition -= vliderPerClickMove;
+        }
+
+        vliderGrid.style.right = vliderPosition + '%';
+      });
     }
   }]);
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,62 @@
 class Vlider {
-    constructor(options) {
-        this.options = {
-            size: 1,
-            direction: 'horizontal',
-            el: '.vlider'
-        }
+    constructor({size = 3, direction = 'horizontal', el = '.vlider', withIndicator = false}) {
+        this.size = size;
+        this.direction = direction;
+        this.el = el;
+        this.withIndicator = withIndicator;
     }
     init() {
-        let vlider = document.querySelector(el);
+        let vlider = document.querySelector(this.el),
+            vliderGrid = document.querySelector('.vlider-grid'),
+            vliderPosition = 0,
+            vliderItems = document.querySelectorAll('.vlider-item'),
+            vliderPerClickMove = 100 / this.size,
+            vliderLimit = (vliderPerClickMove * vliderItems.length) - (vliderPerClickMove * this.size),
+            vliderNext = document.querySelector('.vlider-next'),
+            vliderPrev = document.querySelector('.vlider-prev');
+        // initialize the size of slider items
+        vliderItems.forEach((item) => {
+            item.style.flexBasis = (100 / this.size) + '%';
+        });
+        // function to make vlider indicator icon, and it's clickable
+        const enableIndicator = () => {
+            let result = '';
+            for (let i = 0; i < this.size; i++) {
+                result += `<span class="icon"></span>`;
+            }
+            return result;
+        }
+        if (this.withIndicator) {
+            document.querySelector('.vlider-indicator').innerHTML = enableIndicator();
+            let vliderIndicators = document.querySelectorAll('.vlider-indicator > .icon');
+            vliderIndicators.forEach((indicator,index) => {
+                indicator.addEventListener('click', (event) => {
+                    vliderPosition = (vliderPerClickMove * this.size) * index;
+                    vliderGrid.style.right = vliderPosition + '%';
+                });
+            });
+        }
+        // just for debugging
+        console.log(`
+            element: ${this.el},
+            direction: ${this.direction},
+            size: ${this.size}
+        `)
+        vliderNext.addEventListener('click', (event) => {
+            if (vliderPosition >= vliderLimit) {
+                vliderPosition = 0;
+            } else {
+                vliderPosition += vliderPerClickMove;
+            }
+            vliderGrid.style.right = vliderPosition + '%';
+        })
+        vliderPrev.addEventListener('click', (event) => {
+            if (vliderPosition <= 0) {
+                vliderPosition = vliderLimit;
+            } else {
+                vliderPosition -= vliderPerClickMove;
+            }
+            vliderGrid.style.right = vliderPosition + '%';
+        });
     }
 }
